483c0a496a1119b6b4652d77597cde617da2e510
Pedro Corrêa <pedrocorrea2005@outlook.com>
2024-04-16 20:03:21 -0300

    Create FrmProduto.java
    Criação da classe da tela

---
+++ Aulas/Aula16-04-2024/src/br/unisal/gui/FrmProduto.java
@@ -0,0 +1,21 @@
+package br.unisal.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class FrmProduto extends JFrame implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
