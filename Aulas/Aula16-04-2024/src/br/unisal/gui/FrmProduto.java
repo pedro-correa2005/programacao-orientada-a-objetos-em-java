3ac1770ba40fb99d42119a77a64046e425b19cd6
Pedro Corrêa <pedrocorrea2005@outlook.com>
2024-04-16 20:06:30 -0300

    Update FrmProduto.java
    Declaração dos labels

--- Aulas/Aula16-04-2024/src/br/unisal/gui/FrmProduto.java
+++ Aulas/Aula16-04-2024/src/br/unisal/gui/FrmProduto.java
@@ -4,9 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class FrmProduto extends JFrame implements ActionListener {
-
+	//Declaração dos componentes da interface
+	JLabel lbCodigo, lbDescricao, lbUnidadeMedida, lbLargula, lbComprimento,
+	lbSituacao, lbLocalizacao;
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
