4bf259963a37b155ce0f1018a867ddb909f89ca7
Pedro Corrêa <pedrocorrea2005@outlook.com>
2024-04-16 19:31:18 -0300

    Update UnidadeMedida.java
    Criação de atributos e um método construtor para a Enum UnidadeMedida

--- Aulas/Aula16-04-2024/src/br/unisal/modelagem/UnidadeMedida.java
+++ Aulas/Aula16-04-2024/src/br/unisal/modelagem/UnidadeMedida.java
@@ -1,5 +1,21 @@
 package br.unisal.modelagem;
 
 public enum UnidadeMedida {
-	UN, PC, MT, TON, CX, DZ, LT, RL;
+	UN("UN", "Unidade"),
+	PC("PC", "Peça"),
+	MT("MT", "Metro"),
+	TON("TON", "Tonelada"),
+	CX("CX", "Caixa"),
+	DZ("DZ","Dúzia"),
+	LT("LT", "Litro"),
+	RL("RL", "Rolo");
+	
+	private String unidade;
+	private String descrição;
+	
+	
+	private UnidadeMedida(String unidade, String descrição) {
+		this.unidade = unidade;
+		this.descrição = descrição;
+	}
 }
