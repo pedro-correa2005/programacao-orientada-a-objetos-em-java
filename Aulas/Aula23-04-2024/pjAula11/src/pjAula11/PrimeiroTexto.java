6382af7323f47c982b6eca1fcae877491f4dced6
Pedro Corrêa <pedrocorrea2005@outlook.com>
2024-04-23 20:14:18 -0300

    Testes com criação de diretórios

--- Aulas/Aula23-04-2024/pjAula11/src/pjAula11/PrimeiroTexto.java
+++ Aulas/Aula23-04-2024/pjAula11/src/pjAula11/PrimeiroTexto.java
@@ -8,10 +8,20 @@ public class PrimeiroTexto {
 		String informacoes = "";
 		File arquivo = new File("../pjAula11/MeuPrimeiroArquivo.txt");
 		
-		System.out.println(arquivo.getAbsolutePath());
+		/*System.out.println(arquivo.getAbsolutePath());
 		System.out.println(arquivo.exists());
 		System.out.println(arquivo.canWrite());
 		System.out.println(arquivo.isDirectory());
-		System.out.println(arquivo.isFile());
+		System.out.println(arquivo.isFile());*/
+		
+		File integracao = new File("integra");
+		
+		if(integracao.exists()) {
+			System.out.println("Arquivo existe");
+		}else {
+			integracao.mkdirs();
+		}
+		System.out.println(integracao.exists()?"Existe":"Não existe");
+		
 	}
 }
