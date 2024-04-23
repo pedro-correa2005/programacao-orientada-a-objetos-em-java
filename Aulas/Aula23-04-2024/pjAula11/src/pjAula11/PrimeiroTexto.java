02c7b01e210273ac97464b9e62231ebd21749b30
Pedro Corrêa <pedrocorrea2005@outlook.com>
2024-04-23 19:47:37 -0300

    Criação do arquivo texto

--- Aulas/Aula23-04-2024/pjAula11/src/pjAula11/PrimeiroTexto.java
+++ Aulas/Aula23-04-2024/pjAula11/src/pjAula11/PrimeiroTexto.java
@@ -1,6 +1,7 @@
 package pjAula11;
 
 import java.io.File;
+import java.io.IOException;
 
 public class PrimeiroTexto {
 	public static void main(String[] args) {
@@ -8,5 +9,17 @@ public class PrimeiroTexto {
 		File arquivo = new File("MeuPrimeiroArquivo.txt");
 		
 		System.out.println(arquivo.getAbsolutePath());
+		System.out.println(arquivo.exists());
+		System.out.println(arquivo.canWrite());
+		System.out.println(arquivo.isDirectory());
+		System.out.println(arquivo.isFile());
+		
+		if(!arquivo.exists()) {	
+			try{
+				arquivo.createNewFile();
+			}catch(IOException e) {
+				
+			}
+		}
 	}
 }
