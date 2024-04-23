2527b09b2f830acf8cab1f680c55726178185315
Pedro Corrêa <pedrocorrea2005@outlook.com>
2024-04-23 19:57:59 -0300

    Update PrimeiroTexto.java
    Testando diferentes formas de abrir um arquivo

--- Aulas/Aula23-04-2024/pjAula11/src/pjAula11/PrimeiroTexto.java
+++ Aulas/Aula23-04-2024/pjAula11/src/pjAula11/PrimeiroTexto.java
@@ -6,20 +6,12 @@ import java.io.IOException;
 public class PrimeiroTexto {
 	public static void main(String[] args) {
 		String informacoes = "";
-		File arquivo = new File("MeuPrimeiroArquivo.txt");
+		File arquivo = new File("../pjAula11/MeuPrimeiroArquivo.txt");
 		
 		System.out.println(arquivo.getAbsolutePath());
 		System.out.println(arquivo.exists());
 		System.out.println(arquivo.canWrite());
 		System.out.println(arquivo.isDirectory());
 		System.out.println(arquivo.isFile());
-		
-		if(!arquivo.exists()) {	
-			try{
-				arquivo.createNewFile();
-			}catch(IOException e) {
-				
-			}
-		}
 	}
 }
