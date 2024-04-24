7595ab0e068ee4de743fbce0ee157bcd2760f0c0
Pedro Corrêa <pedrocorrea2005@outlook.com>
2024-04-23 21:58:08 -0300

    Update ListaDiretorios.java

--- Aulas/Aula23-04-2024/pjAula11/src/pjAula11/ListaDiretorios.java
+++ Aulas/Aula23-04-2024/pjAula11/src/pjAula11/ListaDiretorios.java
@@ -4,14 +4,26 @@ import java.io.File;
 
 public class ListaDiretorios {
 	public static void main(String[] args) {
-		File diretorio = new File("C:\\");
+		File diretorio = new File(".");
 		if(diretorio.isDirectory()) {
 			String [] arquivos = diretorio.list();
 			
 			for (int i = 0; i < arquivos.length; i++) {
 				File f = new File(arquivos[i]);
 				System.out.println((f.isDirectory()?"Diretorio ":"Arquivo ") + arquivos[i]);
-				
+				if(f.isDirectory())
+					listar(f);
+			}
+		}
+	}
+	public static void listar(File pasta) {
+		String [] pastas = pasta.list();
+		for (String s : pastas) {
+			File arquivo = new File(s);
+			if(arquivo.isDirectory()) {
+				listar(arquivo);
+			}else {
+				System.out.println("\tArquivo: " + arquivo.getName());
 			}
 		}
 	}
