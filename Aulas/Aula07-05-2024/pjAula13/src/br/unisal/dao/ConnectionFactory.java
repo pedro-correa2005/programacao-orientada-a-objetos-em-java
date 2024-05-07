c6407b9eb7a776aefb22b524649a1e8d81285ff0
Pedro Corrêa <pedrocorrea2005@outlook.com>
2024-05-07 20:39:23 -0300

    pjAula13

---
+++ Aulas/Aula07-05-2024/pjAula13/src/br/unisal/dao/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package br.unisal.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public static Connection getConnection() throws SQLException{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/aula",
+					"root",
+					"12305"
+					);
+		}catch(ClassNotFoundException erro) {
+			throw new SQLException("Houve um erro, não foi possível a conexão "
+					+ erro);
+		}
+	}
+}
